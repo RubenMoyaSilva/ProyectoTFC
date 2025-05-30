e7c1866f7391da731ef8cebe9439b41b1941f77e
Rubén Moya <150965065+RubenMoyaSilva@users.noreply.github.com>
2025-05-30 21:35:55 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -6,7 +6,7 @@ $error = $_GET['error'] ?? null;
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Login - PixelTec</title>
+  <title></title>
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="script.js" defer></script>
